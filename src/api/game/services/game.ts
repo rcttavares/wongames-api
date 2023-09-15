4de81532d5b426e84ceef6d0413a3d1b47d71368
Rafael Tavares <rafael.tawares@gmail.com>
2023-09-15 18:03:43 -0300

    Fetch game data from GoG

--- src/api/game/services/game.ts
+++ src/api/game/services/game.ts
@@ -1,7 +1,17 @@
 /**
  * game service
  */
+import axios from "axios";
+import { factories } from "@strapi/strapi";
 
-import { factories } from '@strapi/strapi';
+export default factories.createCoreService("api::game.game", () => ({
+  async populate(params) {
+    const apiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game?sort=rating`;
 
-export default factories.createCoreService('api::game.game');
+    const {
+      data: { products },
+    } = await axios.get(apiUrl);
+
+    console.log(products[1]);
+  },
+}));
